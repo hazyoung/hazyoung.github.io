9e8761e593eb20b13c639051554fec2a36edc4c7
hazyoung <hgyou1@student.monash.edu>
2017-05-09 15:19:02 +1000

    update

---
+++ hover/assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+
+
+  $('.zoom').mouseenter(function() {
+    $('.thumbnail').addClass('enlarge');
+  });
+
+  $('.zoom').mouseleave(function() {
+    $('.thumbnail').removeClass('enlarge');
+  });
+
+
+});
